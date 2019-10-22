c27e64678b01ff1d9eb21961da37bcdf975cf7b8
Tan Wei Lian <twl_william89@hotmail.com>
2019-10-23 01:34:40 +0800

    added more features

--- dataLoader.py
+++ dataLoader.py
@@ -2,7 +2,87 @@ import os
 import datetime
 import random
 import pandas as pd 
-data_folder = os.path.join("geolife-trajectory-1.3", "Data")
+import numpy as np
+from vincenty import vincenty
+
+THRESHOLD = 30
+
+def calculate_speed(current, next):
+    if next is None:
+        return 0
+    distance = 1000*vincenty((current["latitude"],current["longitude"]),(next["latitude"],next["longitude"]))
+    time = (next["timestamp"]-current["timestamp"]).seconds
+    if time <= 0:
+        time = 1
+    if time > THRESHOLD:
+        return 0
+    #print("{0} {1} {2}".format(current["timestamp"], next["timestamp"], next["timestamp"]-current["timestamp"]))
+    #print("{0} {1}".format(distance, time))
+    return distance /time
+def calculate_acceleration(current, next):
+    if next is None:
+        return 0
+    speed_diff = next["speed"] - current["speed"]
+    time = (next["timestamp"]-current["timestamp"]).seconds
+    if time <= 0:
+        time = 1
+    if time > THRESHOLD:
+        return 0
+    return speed_diff / time
+
+def calculate_jerk(current, next):
+    if next is None:
+        return 0
+    acc_diff = next["acceleration"] - current["acceleration"]
+    time = (next["timestamp"] - current["timestamp"]).seconds
+    if time <= 0:
+        time = 1
+    if time > THRESHOLD:
+        return 0
+    return acc_diff / time
+def calculate_bearing(current, next):
+    if next is None:
+        return 0
+    time = (next["timestamp"] - current["timestamp"]).seconds
+    if time <= 0:
+        time = 1
+    if time > THRESHOLD:
+        return 0
+    xx = np.cos(current["latitude"] * np.pi/180) * np.sin(next["latitude"] * np.pi/180) - np.sin(current["latitude"] * np.pi/180) * np.cos(next["latitude"] * np.pi/180) * np.cos((next["longitude"] - current["longitude"]) * np.pi/180)
+    yy = np.sin(next["longitude"] - current["latitude"] * np.pi/180) * np.cos(next["latitude"] * np.pi/180)
+    return np.arctan2(yy,xx) * 180/np.pi
+def y(x):
+    try:
+        yy = np.sin((x[8] - x[0]) * np.pi/180) *np.cos( x[7]* np.pi/180)
+    except:
+        yy= np.nan
+    return yy
+def x(x):
+    try:
+        xx = np.cos(x[0] * np.pi/180) *np.sin(x[7]* np.pi/180) - np.sin(x[0]* np.pi/180) * np.cos(x[7]* np.pi/180)*np.cos((x[8]-x[1])* np.pi/180)
+    except:
+        xx = np.nan
+    return xx
+def preprocess(data_points):
+    data_points.sort(key=lambda x: x["timestamp"])
+    for i in range(0, len(data_points)):
+        j = i+1
+        next = data_points[j] if j < len(data_points) else None
+        data_points[i]["speed"] = calculate_speed(data_points[i], next)
+        
+    for i in range(0, len(data_points)):
+        j = i+1
+        next = data_points[j] if j < len(data_points) else None
+        data_points[i]["acceleration"] = calculate_acceleration(data_points[i], next)
+    for i in range(0, len(data_points)):
+        j = i+1
+        next = data_points[j] if j < len(data_points) else None
+        data_points[i]["jerk"] = calculate_jerk(data_points[i], next) 
+    for i in range(0, len(data_points)):
+        j = i+1
+        next = data_points[j] if j < len(data_points) else None
+        data_points[i]["bearing"] = calculate_bearing(data_points[i], next) 
+    return data_points
 
 class User:
     def __init__(self, name, path):
@@ -19,7 +99,7 @@ class User:
                         start_time = datetime.datetime.strptime(splits[0] + " " + splits[1], "%Y/%m/%d %H:%M:%S")
                         end_time = datetime.datetime.strptime(splits[2] + " " + splits[3], "%Y/%m/%d %H:%M:%S")
                         label = splits[4]
-                        self.labels.append({"start_time": start_time, "end_time": end_time, "label": label})
+                        self.labels.append({"user": self.name, "start_time": start_time, "end_time": end_time, "label": label})
         
         self.data = []
         traj_path = os.path.join(self.path, "Trajectory")
@@ -35,7 +115,7 @@ class User:
                         altitude = float(splits[3])
                         day_since = float(splits[4])#unused 
                         timestamp = datetime.datetime.strptime(splits[5] + " " + splits[6], "%Y-%m-%d %H:%M:%S")
-                        self.data.append({ "timestamp": timestamp, "latitude": latitude, "longitude": longitude, "altitude": altitude})
+                        self.data.append({"user": self.name, "timestamp": timestamp, "latitude": latitude, "longitude": longitude, "altitude": altitude})
         print("loaded {0}".format(self.path))
 
 class Dataloader:
@@ -48,7 +128,10 @@ class Dataloader:
             if gen <= load_portion:
                 u = User(user_folder, os.path.join(data_dir, user_folder))
                 self.labels += u.labels
-                self.points += u.data
+                self.points += preprocess(u.data)
                 
     def getDataFrames(self):
         return pd.DataFrame(self.points), pd.DataFrame(self.labels)
+
+points, labels = Dataloader(load_portion=0.02).getDataFrames()
+print(points)
--- pointFeatures.py
+++ pointFeatures.py
@@ -1,3 +1,4 @@
 from dataLoader import Dataloader
+from vincenty import vincenty
 
-points, labels = Dataloader(load_portion=0.01).getDataFrames()
+#points, labels = Dataloader(load_portion=0.01).getDataFrames()
